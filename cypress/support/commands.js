18788a22b7b638b2175849f9379c7f4f9800db74
Denys <leldenis8@gmail.com>
2025-05-06 14:44:15 +0300

    registration form tests added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,55 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+    if (options && options.sensitive) {
+      options.log = false
+      Cypress.log({
+        $el: element,
+        name: 'type',
+        message: '*'.repeat(text.length),
+      })
+    }
+  
+    return originalFn(element, text, options)
+  });
+
+Cypress.Commands.add('generateUniqueEmail', (baseEmail) => {
+  return `${baseEmail.split('@')[0]}+${Date.now()}@${baseEmail.split('@')[1]}`;
+});
+
+Cypress.Commands.add('generateValidPassword', () => {
+  const length = Cypress._.random(8, 15);
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const numberChars = '0123456789';
+
+  let password = '';
+  password += uppercaseChars.charAt(Cypress._.random(0, uppercaseChars.length - 1));
+  password += lowercaseChars.charAt(Cypress._.random(0, lowercaseChars.length - 1));
+  password += numberChars.charAt(Cypress._.random(0, numberChars.length - 1));
+
+  const remainingLength = length - password.length;
+  for (let i = 0; i < remainingLength; i++) {
+    const charType = Cypress._.random(0, 2);
+    switch (charType) {
+      case 0:
+        password += uppercaseChars.charAt(Cypress._.random(0, uppercaseChars.length - 1));
+        break;
+      case 1:
+        password += lowercaseChars.charAt(Cypress._.random(0, lowercaseChars.length - 1));
+        break;
+      case 2:
+        password += numberChars.charAt(Cypress._.random(0, numberChars.length - 1));
+        break;
+    }
+  }
+
+  password = Cypress._.shuffle(password.split('')).join('');
+  return cy.wrap(password);
+});
+
+Cypress.Commands.add('login', () => {
+  cy.visit('https://guest:welcome2qauto@qauto.forstudy.space');
+  cy.get('.header_signin').contains('Sign In').click();
+  cy.get('#signinEmail').type('welcome2qauto@qauto'); 
+  cy.get('#signinPassword').type('ValidPassrd1', { sensitive: true }); 
+  cy.contains('button', 'Login').click();
+});
